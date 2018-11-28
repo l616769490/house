7f58f6c8437944196bb28187b69894c58978f968
l616769490 <l616769490@126.com>
2018-11-28 17:30:22 +0800

    HBaseBean

---
+++ src/main/java/com/tecode/house/d01/bean/HBaseBean.java
@@ -0,0 +1,201 @@
+package com.tecode.house.d01.bean; 
+public class HBaseBean { 
+private String CONTROL;public String getCONTROL() { return CONTROL;} 
+public void setCONTROL(String CONTROL) {this.CONTROL = CONTROL;}
+private String AGE1;public String getAGE1() { return AGE1;} 
+public void setAGE1(String AGE1) {this.AGE1 = AGE1;}
+private String METRO3;public String getMETRO3() { return METRO3;} 
+public void setMETRO3(String METRO3) {this.METRO3 = METRO3;}
+private String REGION;public String getREGION() { return REGION;} 
+public void setREGION(String REGION) {this.REGION = REGION;}
+private String LMED;public String getLMED() { return LMED;} 
+public void setLMED(String LMED) {this.LMED = LMED;}
+private String FMR;public String getFMR() { return FMR;} 
+public void setFMR(String FMR) {this.FMR = FMR;}
+private String L30;public String getL30() { return L30;} 
+public void setL30(String L30) {this.L30 = L30;}
+private String L50;public String getL50() { return L50;} 
+public void setL50(String L50) {this.L50 = L50;}
+private String L80;public String getL80() { return L80;} 
+public void setL80(String L80) {this.L80 = L80;}
+private String IPOV;public String getIPOV() { return IPOV;} 
+public void setIPOV(String IPOV) {this.IPOV = IPOV;}
+private String BEDRMS;public String getBEDRMS() { return BEDRMS;} 
+public void setBEDRMS(String BEDRMS) {this.BEDRMS = BEDRMS;}
+private String BUILT;public String getBUILT() { return BUILT;} 
+public void setBUILT(String BUILT) {this.BUILT = BUILT;}
+private String STATUS;public String getSTATUS() { return STATUS;} 
+public void setSTATUS(String STATUS) {this.STATUS = STATUS;}
+private String TYPE;public String getTYPE() { return TYPE;} 
+public void setTYPE(String TYPE) {this.TYPE = TYPE;}
+private String VALUE;public String getVALUE() { return VALUE;} 
+public void setVALUE(String VALUE) {this.VALUE = VALUE;}
+private String VACANCY;public String getVACANCY() { return VACANCY;} 
+public void setVACANCY(String VACANCY) {this.VACANCY = VACANCY;}
+private String TENURE;public String getTENURE() { return TENURE;} 
+public void setTENURE(String TENURE) {this.TENURE = TENURE;}
+private String NUNITS;public String getNUNITS() { return NUNITS;} 
+public void setNUNITS(String NUNITS) {this.NUNITS = NUNITS;}
+private String ROOMS;public String getROOMS() { return ROOMS;} 
+public void setROOMS(String ROOMS) {this.ROOMS = ROOMS;}
+private String WEIGHT;public String getWEIGHT() { return WEIGHT;} 
+public void setWEIGHT(String WEIGHT) {this.WEIGHT = WEIGHT;}
+private String PER;public String getPER() { return PER;} 
+public void setPER(String PER) {this.PER = PER;}
+private String ZINC2;public String getZINC2() { return ZINC2;} 
+public void setZINC2(String ZINC2) {this.ZINC2 = ZINC2;}
+private String ZADEQ;public String getZADEQ() { return ZADEQ;} 
+public void setZADEQ(String ZADEQ) {this.ZADEQ = ZADEQ;}
+private String ZSMHC;public String getZSMHC() { return ZSMHC;} 
+public void setZSMHC(String ZSMHC) {this.ZSMHC = ZSMHC;}
+private String STRUCTURETYPE;public String getSTRUCTURETYPE() { return STRUCTURETYPE;} 
+public void setSTRUCTURETYPE(String STRUCTURETYPE) {this.STRUCTURETYPE = STRUCTURETYPE;}
+private String OWNRENT;public String getOWNRENT() { return OWNRENT;} 
+public void setOWNRENT(String OWNRENT) {this.OWNRENT = OWNRENT;}
+private String UTILITY;public String getUTILITY() { return UTILITY;} 
+public void setUTILITY(String UTILITY) {this.UTILITY = UTILITY;}
+private String OTHERCOST;public String getOTHERCOST() { return OTHERCOST;} 
+public void setOTHERCOST(String OTHERCOST) {this.OTHERCOST = OTHERCOST;}
+private String COST06;public String getCOST06() { return COST06;} 
+public void setCOST06(String COST06) {this.COST06 = COST06;}
+private String COST12;public String getCOST12() { return COST12;} 
+public void setCOST12(String COST12) {this.COST12 = COST12;}
+private String COST08;public String getCOST08() { return COST08;} 
+public void setCOST08(String COST08) {this.COST08 = COST08;}
+private String COSTMED;public String getCOSTMED() { return COSTMED;} 
+public void setCOSTMED(String COSTMED) {this.COSTMED = COSTMED;}
+private String TOTSAL;public String getTOTSAL() { return TOTSAL;} 
+public void setTOTSAL(String TOTSAL) {this.TOTSAL = TOTSAL;}
+private String ASSISTED;public String getASSISTED() { return ASSISTED;} 
+public void setASSISTED(String ASSISTED) {this.ASSISTED = ASSISTED;}
+private String GLMED;public String getGLMED() { return GLMED;} 
+public void setGLMED(String GLMED) {this.GLMED = GLMED;}
+private String GL30;public String getGL30() { return GL30;} 
+public void setGL30(String GL30) {this.GL30 = GL30;}
+private String GL50;public String getGL50() { return GL50;} 
+public void setGL50(String GL50) {this.GL50 = GL50;}
+private String GL80;public String getGL80() { return GL80;} 
+public void setGL80(String GL80) {this.GL80 = GL80;}
+private String APLMED;public String getAPLMED() { return APLMED;} 
+public void setAPLMED(String APLMED) {this.APLMED = APLMED;}
+private String ABL30;public String getABL30() { return ABL30;} 
+public void setABL30(String ABL30) {this.ABL30 = ABL30;}
+private String ABL50;public String getABL50() { return ABL50;} 
+public void setABL50(String ABL50) {this.ABL50 = ABL50;}
+private String ABL80;public String getABL80() { return ABL80;} 
+public void setABL80(String ABL80) {this.ABL80 = ABL80;}
+private String ABLMED;public String getABLMED() { return ABLMED;} 
+public void setABLMED(String ABLMED) {this.ABLMED = ABLMED;}
+private String BURDEN;public String getBURDEN() { return BURDEN;} 
+public void setBURDEN(String BURDEN) {this.BURDEN = BURDEN;}
+private String INCRELAMIPCT;public String getINCRELAMIPCT() { return INCRELAMIPCT;} 
+public void setINCRELAMIPCT(String INCRELAMIPCT) {this.INCRELAMIPCT = INCRELAMIPCT;}
+private String INCRELAMICAT;public String getINCRELAMICAT() { return INCRELAMICAT;} 
+public void setINCRELAMICAT(String INCRELAMICAT) {this.INCRELAMICAT = INCRELAMICAT;}
+private String INCRELPOVPCT;public String getINCRELPOVPCT() { return INCRELPOVPCT;} 
+public void setINCRELPOVPCT(String INCRELPOVPCT) {this.INCRELPOVPCT = INCRELPOVPCT;}
+private String INCRELPOVCAT;public String getINCRELPOVCAT() { return INCRELPOVCAT;} 
+public void setINCRELPOVCAT(String INCRELPOVCAT) {this.INCRELPOVCAT = INCRELPOVCAT;}
+private String INCRELFMRPCT;public String getINCRELFMRPCT() { return INCRELFMRPCT;} 
+public void setINCRELFMRPCT(String INCRELFMRPCT) {this.INCRELFMRPCT = INCRELFMRPCT;}
+private String INCRELFMRCAT;public String getINCRELFMRCAT() { return INCRELFMRCAT;} 
+public void setINCRELFMRCAT(String INCRELFMRCAT) {this.INCRELFMRCAT = INCRELFMRCAT;}
+private String COST06RELAMIPCT;public String getCOST06RELAMIPCT() { return COST06RELAMIPCT;} 
+public void setCOST06RELAMIPCT(String COST06RELAMIPCT) {this.COST06RELAMIPCT = COST06RELAMIPCT;}
+private String COST06RELAMICAT;public String getCOST06RELAMICAT() { return COST06RELAMICAT;} 
+public void setCOST06RELAMICAT(String COST06RELAMICAT) {this.COST06RELAMICAT = COST06RELAMICAT;}
+private String COST06RELPOVPCT;public String getCOST06RELPOVPCT() { return COST06RELPOVPCT;} 
+public void setCOST06RELPOVPCT(String COST06RELPOVPCT) {this.COST06RELPOVPCT = COST06RELPOVPCT;}
+private String COST06RELPOVCAT;public String getCOST06RELPOVCAT() { return COST06RELPOVCAT;} 
+public void setCOST06RELPOVCAT(String COST06RELPOVCAT) {this.COST06RELPOVCAT = COST06RELPOVCAT;}
+private String COST06RELFMRPCT;public String getCOST06RELFMRPCT() { return COST06RELFMRPCT;} 
+public void setCOST06RELFMRPCT(String COST06RELFMRPCT) {this.COST06RELFMRPCT = COST06RELFMRPCT;}
+private String COST06RELFMRCAT;public String getCOST06RELFMRCAT() { return COST06RELFMRCAT;} 
+public void setCOST06RELFMRCAT(String COST06RELFMRCAT) {this.COST06RELFMRCAT = COST06RELFMRCAT;}
+private String COST08RELAMIPCT;public String getCOST08RELAMIPCT() { return COST08RELAMIPCT;} 
+public void setCOST08RELAMIPCT(String COST08RELAMIPCT) {this.COST08RELAMIPCT = COST08RELAMIPCT;}
+private String COST08RELAMICAT;public String getCOST08RELAMICAT() { return COST08RELAMICAT;} 
+public void setCOST08RELAMICAT(String COST08RELAMICAT) {this.COST08RELAMICAT = COST08RELAMICAT;}
+private String COST08RELPOVPCT;public String getCOST08RELPOVPCT() { return COST08RELPOVPCT;} 
+public void setCOST08RELPOVPCT(String COST08RELPOVPCT) {this.COST08RELPOVPCT = COST08RELPOVPCT;}
+private String COST08RELPOVCAT;public String getCOST08RELPOVCAT() { return COST08RELPOVCAT;} 
+public void setCOST08RELPOVCAT(String COST08RELPOVCAT) {this.COST08RELPOVCAT = COST08RELPOVCAT;}
+private String COST08RELFMRPCT;public String getCOST08RELFMRPCT() { return COST08RELFMRPCT;} 
+public void setCOST08RELFMRPCT(String COST08RELFMRPCT) {this.COST08RELFMRPCT = COST08RELFMRPCT;}
+private String COST08RELFMRCAT;public String getCOST08RELFMRCAT() { return COST08RELFMRCAT;} 
+public void setCOST08RELFMRCAT(String COST08RELFMRCAT) {this.COST08RELFMRCAT = COST08RELFMRCAT;}
+private String COST12RELAMIPCT;public String getCOST12RELAMIPCT() { return COST12RELAMIPCT;} 
+public void setCOST12RELAMIPCT(String COST12RELAMIPCT) {this.COST12RELAMIPCT = COST12RELAMIPCT;}
+private String COST12RELAMICAT;public String getCOST12RELAMICAT() { return COST12RELAMICAT;} 
+public void setCOST12RELAMICAT(String COST12RELAMICAT) {this.COST12RELAMICAT = COST12RELAMICAT;}
+private String COST12RELPOVPCT;public String getCOST12RELPOVPCT() { return COST12RELPOVPCT;} 
+public void setCOST12RELPOVPCT(String COST12RELPOVPCT) {this.COST12RELPOVPCT = COST12RELPOVPCT;}
+private String COST12RELPOVCAT;public String getCOST12RELPOVCAT() { return COST12RELPOVCAT;} 
+public void setCOST12RELPOVCAT(String COST12RELPOVCAT) {this.COST12RELPOVCAT = COST12RELPOVCAT;}
+private String COST12RELFMRPCT;public String getCOST12RELFMRPCT() { return COST12RELFMRPCT;} 
+public void setCOST12RELFMRPCT(String COST12RELFMRPCT) {this.COST12RELFMRPCT = COST12RELFMRPCT;}
+private String COST12RELFMRCAT;public String getCOST12RELFMRCAT() { return COST12RELFMRCAT;} 
+public void setCOST12RELFMRCAT(String COST12RELFMRCAT) {this.COST12RELFMRCAT = COST12RELFMRCAT;}
+private String COSTMedRELAMIPCT;public String getCOSTMedRELAMIPCT() { return COSTMedRELAMIPCT;} 
+public void setCOSTMedRELAMIPCT(String COSTMedRELAMIPCT) {this.COSTMedRELAMIPCT = COSTMedRELAMIPCT;}
+private String COSTMedRELAMICAT;public String getCOSTMedRELAMICAT() { return COSTMedRELAMICAT;} 
+public void setCOSTMedRELAMICAT(String COSTMedRELAMICAT) {this.COSTMedRELAMICAT = COSTMedRELAMICAT;}
+private String COSTMedRELPOVPCT;public String getCOSTMedRELPOVPCT() { return COSTMedRELPOVPCT;} 
+public void setCOSTMedRELPOVPCT(String COSTMedRELPOVPCT) {this.COSTMedRELPOVPCT = COSTMedRELPOVPCT;}
+private String COSTMedRELPOVCAT;public String getCOSTMedRELPOVCAT() { return COSTMedRELPOVCAT;} 
+public void setCOSTMedRELPOVCAT(String COSTMedRELPOVCAT) {this.COSTMedRELPOVCAT = COSTMedRELPOVCAT;}
+private String COSTMedRELFMRPCT;public String getCOSTMedRELFMRPCT() { return COSTMedRELFMRPCT;} 
+public void setCOSTMedRELFMRPCT(String COSTMedRELFMRPCT) {this.COSTMedRELFMRPCT = COSTMedRELFMRPCT;}
+private String COSTMedRELFMRCAT;public String getCOSTMedRELFMRCAT() { return COSTMedRELFMRCAT;} 
+public void setCOSTMedRELFMRCAT(String COSTMedRELFMRCAT) {this.COSTMedRELFMRCAT = COSTMedRELFMRCAT;}
+private String FMTZADEQ;public String getFMTZADEQ() { return FMTZADEQ;} 
+public void setFMTZADEQ(String FMTZADEQ) {this.FMTZADEQ = FMTZADEQ;}
+private String FMTMETRO3;public String getFMTMETRO3() { return FMTMETRO3;} 
+public void setFMTMETRO3(String FMTMETRO3) {this.FMTMETRO3 = FMTMETRO3;}
+private String FMTBUILT;public String getFMTBUILT() { return FMTBUILT;} 
+public void setFMTBUILT(String FMTBUILT) {this.FMTBUILT = FMTBUILT;}
+private String FMTSTRUCTURETYPE;public String getFMTSTRUCTURETYPE() { return FMTSTRUCTURETYPE;} 
+public void setFMTSTRUCTURETYPE(String FMTSTRUCTURETYPE) {this.FMTSTRUCTURETYPE = FMTSTRUCTURETYPE;}
+private String FMTBEDRMS;public String getFMTBEDRMS() { return FMTBEDRMS;} 
+public void setFMTBEDRMS(String FMTBEDRMS) {this.FMTBEDRMS = FMTBEDRMS;}
+private String FMTOWNRENT;public String getFMTOWNRENT() { return FMTOWNRENT;} 
+public void setFMTOWNRENT(String FMTOWNRENT) {this.FMTOWNRENT = FMTOWNRENT;}
+private String FMTCOST06RELPOVCAT;public String getFMTCOST06RELPOVCAT() { return FMTCOST06RELPOVCAT;} 
+public void setFMTCOST06RELPOVCAT(String FMTCOST06RELPOVCAT) {this.FMTCOST06RELPOVCAT = FMTCOST06RELPOVCAT;}
+private String FMTCOST08RELPOVCAT;public String getFMTCOST08RELPOVCAT() { return FMTCOST08RELPOVCAT;} 
+public void setFMTCOST08RELPOVCAT(String FMTCOST08RELPOVCAT) {this.FMTCOST08RELPOVCAT = FMTCOST08RELPOVCAT;}
+private String FMTCOST12RELPOVCAT;public String getFMTCOST12RELPOVCAT() { return FMTCOST12RELPOVCAT;} 
+public void setFMTCOST12RELPOVCAT(String FMTCOST12RELPOVCAT) {this.FMTCOST12RELPOVCAT = FMTCOST12RELPOVCAT;}
+private String FMTCOSTMEDRELPOVCAT;public String getFMTCOSTMEDRELPOVCAT() { return FMTCOSTMEDRELPOVCAT;} 
+public void setFMTCOSTMEDRELPOVCAT(String FMTCOSTMEDRELPOVCAT) {this.FMTCOSTMEDRELPOVCAT = FMTCOSTMEDRELPOVCAT;}
+private String FMTINCRELPOVCAT;public String getFMTINCRELPOVCAT() { return FMTINCRELPOVCAT;} 
+public void setFMTINCRELPOVCAT(String FMTINCRELPOVCAT) {this.FMTINCRELPOVCAT = FMTINCRELPOVCAT;}
+private String FMTCOST06RELFMRCAT;public String getFMTCOST06RELFMRCAT() { return FMTCOST06RELFMRCAT;} 
+public void setFMTCOST06RELFMRCAT(String FMTCOST06RELFMRCAT) {this.FMTCOST06RELFMRCAT = FMTCOST06RELFMRCAT;}
+private String FMTCOST08RELFMRCAT;public String getFMTCOST08RELFMRCAT() { return FMTCOST08RELFMRCAT;} 
+public void setFMTCOST08RELFMRCAT(String FMTCOST08RELFMRCAT) {this.FMTCOST08RELFMRCAT = FMTCOST08RELFMRCAT;}
+private String FMTCOST12RELFMRCAT;public String getFMTCOST12RELFMRCAT() { return FMTCOST12RELFMRCAT;} 
+public void setFMTCOST12RELFMRCAT(String FMTCOST12RELFMRCAT) {this.FMTCOST12RELFMRCAT = FMTCOST12RELFMRCAT;}
+private String FMTCOSTMEDRELFMRCAT;public String getFMTCOSTMEDRELFMRCAT() { return FMTCOSTMEDRELFMRCAT;} 
+public void setFMTCOSTMEDRELFMRCAT(String FMTCOSTMEDRELFMRCAT) {this.FMTCOSTMEDRELFMRCAT = FMTCOSTMEDRELFMRCAT;}
+private String FMTINCRELFMRCAT;public String getFMTINCRELFMRCAT() { return FMTINCRELFMRCAT;} 
+public void setFMTINCRELFMRCAT(String FMTINCRELFMRCAT) {this.FMTINCRELFMRCAT = FMTINCRELFMRCAT;}
+private String FMTCOST06RELAMICAT;public String getFMTCOST06RELAMICAT() { return FMTCOST06RELAMICAT;} 
+public void setFMTCOST06RELAMICAT(String FMTCOST06RELAMICAT) {this.FMTCOST06RELAMICAT = FMTCOST06RELAMICAT;}
+private String FMTCOST08RELAMICAT;public String getFMTCOST08RELAMICAT() { return FMTCOST08RELAMICAT;} 
+public void setFMTCOST08RELAMICAT(String FMTCOST08RELAMICAT) {this.FMTCOST08RELAMICAT = FMTCOST08RELAMICAT;}
+private String FMTCOST12RELAMICAT;public String getFMTCOST12RELAMICAT() { return FMTCOST12RELAMICAT;} 
+public void setFMTCOST12RELAMICAT(String FMTCOST12RELAMICAT) {this.FMTCOST12RELAMICAT = FMTCOST12RELAMICAT;}
+private String FMTCOSTMEDRELAMICAT;public String getFMTCOSTMEDRELAMICAT() { return FMTCOSTMEDRELAMICAT;} 
+public void setFMTCOSTMEDRELAMICAT(String FMTCOSTMEDRELAMICAT) {this.FMTCOSTMEDRELAMICAT = FMTCOSTMEDRELAMICAT;}
+private String FMTINCRELAMICAT;public String getFMTINCRELAMICAT() { return FMTINCRELAMICAT;} 
+public void setFMTINCRELAMICAT(String FMTINCRELAMICAT) {this.FMTINCRELAMICAT = FMTINCRELAMICAT;}
+private String FMTASSISTED;public String getFMTASSISTED() { return FMTASSISTED;} 
+public void setFMTASSISTED(String FMTASSISTED) {this.FMTASSISTED = FMTASSISTED;}
+private String FMTBURDEN;public String getFMTBURDEN() { return FMTBURDEN;} 
+public void setFMTBURDEN(String FMTBURDEN) {this.FMTBURDEN = FMTBURDEN;}
+private String FMTREGION;public String getFMTREGION() { return FMTREGION;} 
+public void setFMTREGION(String FMTREGION) {this.FMTREGION = FMTREGION;}
+private String FMTSTATUS;public String getFMTSTATUS() { return FMTSTATUS;} 
+public void setFMTSTATUS(String FMTSTATUS) {this.FMTSTATUS = FMTSTATUS;}
+}
