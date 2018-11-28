c40803c74468ad26dbd89f91e2a2d98f094badc5
l616769490 <l616769490@126.com>
2018-11-28 16:51:24 +0800

    新分支

---
+++ src/main/java/com/tecode/house/lijin/utils/CreateHBaseBean.java
@@ -0,0 +1,12 @@
+package com.tecode.house.lijin.utils;
+
+/**
+ * 创建HBaseBean
+ */
+public class CreateHBaseBean {
+    public static void main(String[] args) {
+        System.out.println(CreateHBaseBean.class.getClassLoader().getResource("/").getPath());
+    }
+
+
+}
--- src/main/java/com/tecode/house/zouchao/dao/MySQLScalaDao.scala
+++
@@ -1,5 +0,0 @@
-package com.tecode.house.zouchao.dao
-
-trait MySQLScalaDao {
-
-}
