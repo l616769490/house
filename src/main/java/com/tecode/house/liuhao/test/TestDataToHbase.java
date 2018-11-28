3f84a87e27467cf06c4c4e33b745ff3777997388
935800284 <935800284@qq.com>
2018-11-28 18:01:37 +0800

    1128liuhao

---
+++ src/main/java/com/tecode/house/liuhao/test/TestDataToHbase.java
@@ -0,0 +1,17 @@
+package com.tecode.house.liuhao.test;
+
+import com.tecode.house.liuhao.utils.DataToHbase;
+
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2018/11/28.
+ */
+public class TestDataToHbase {
+    public static void main(String[] args) throws IOException {
+        DataToHbase t = new DataToHbase();
+        t.readFile();
+
+
+    }
+}
