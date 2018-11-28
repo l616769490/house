67aeba18f52f00d377c52ffc14a1a5d8704762c2
mayun <10000@qq.com>
2018-11-28 17:51:51 +0800

    美国住房系统

--- src/main/java/com/tecode/house/lijin/hbase/HBaseFromFile.java
+++ src/main/java/com/tecode/house/lijin/hbase/HBaseFromFile.java
@@ -1,25 +1,12 @@
-package com.tecode.fileimport;
+package com.tecode.house.lijin.hbase;
 
 import com.tecode.house.lijin.utils.ConfigUtil;
 import com.tecode.house.lijin.utils.HBaseUtil;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.util.Bytes;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-/**
- * 文件导入到HBase
- */
-public class FileToHBase {
-
-    /**
-     * 缓冲区大小
-     */
-    private static final int MAX_NUM = 1024;
+public class HBaseFromFile {
     /**
      * 列名
      */
@@ -35,34 +22,35 @@ public class FileToHBase {
      */
     private String[] infos = ConfigUtil.get("hbase_infos").split(",");
 
-    private List<Put> puts = new ArrayList<>(MAX_NUM);
 
     /**
-     * 计数器
+     * 文件路径
      */
-    private static long count = 0;
+    private String path;
+
+    public HBaseFromFile(String path) {
+        this.path = path;
+    }
 
     /**
      * 从文件中导入数据到HBase，文件头部需要包含列名信息，列名必须是英文字母
      *
-     * @param path 文件路径
-     * @param del  如果表存在是否删除表
+     * @param del 如果表存在是否删除表
      * @return 是否成功
      */
-    public boolean fileToHbase(String path, boolean del) {
-        return fileToHbase(path, null, del);
+    public boolean fileToHbase(boolean del) {
+        return fileToHbase(null, del);
     }
 
 
     /**
      * 从文件中导入数据到HBase
      *
-     * @param path   文件路径
      * @param fields 列名
      * @param del    如果表存在是否删除表
      * @return 是否成功
      */
-    public boolean fileToHbase(String path, String[] fields, boolean del) {
+    public boolean fileToHbase(String[] fields, boolean del) {
         try {
             // 获取文件名
             String fileName = path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.'));
@@ -73,6 +61,10 @@ public class FileToHBase {
                 this.fields = fields;
             } else {
                 setFields(br);
+            }
+
+            if (br != null) {
+                br.close();
             }
 
             // 获取表名
@@ -89,18 +81,25 @@ public class FileToHBase {
             }
 
             // 创建表
-            HBaseUtil.createTable(tableName, infos);
+            HBaseUtil.createTable(ConfigUtil.get("hbase_namespace") + ":" + fileName, infos);
+
+            // 关闭连接
+            HBaseUtil.closeConn();
 
             // 将数据写入表中
-            write(br);
+            writeData();
 
-            if (br != null) {
-                br.close();
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return true;
+    }
+
+    /**
+     * 写入数据
+     */
+    private void writeData() {
+
     }
 
     /**
@@ -121,64 +120,11 @@ public class FileToHBase {
         fields = line.split(",");
     }
 
-    /**
-     * 写入数据
-     *
-     * @param br 输入流
-     */
-    private void write(BufferedReader br) {
-
-        br.lines()
-                // 过滤空字符串
-                .filter(s -> s != null && !s.isEmpty())
-                // 拆分并去除首尾空格和引号
-                .map(s -> s.split(",")).map(FileToHBase::trim)
-                .forEach(line -> insert(line));
-        if (puts.size() > 0) {
-            HBaseUtil.addDatas(tableName, puts);
-        }
+    public static void main(String[] args) {
+        String path = HBaseFromFile.class.getClassLoader().getResource("").getPath() + "../notebook/upload/2013.csv";
+        new HBaseFromFile(path).fileToHbase(true);
     }
 
-    /**
-     * 插入一行数据
-     *
-     * @param line 数据
-     */
-    private void insert(String[] line) {
-        if (fields == null || line == null || fields.length != line.length || tableName == null || tableName.isEmpty() || infos == null) {
-            return;
-        }
-
-        count++;
-        if(count % 1000 == 0) {
-            System.out.println("已插入：" + count + "行");
-        }
-
-        // rowKey
-        String rowKey = line[2] + "_" + line[0];
-        // 列族
-        String cf = infos[0];
-        Put put = new Put(Bytes.toBytes(rowKey));
-        for (int i = 0; i < line.length; i++) {
-            // 列名
-            String columnName = fields[i];
-            // 判断是否是cost或fmt列族
-            for (int j = 1; j < infos.length; j++) {
-                if (columnName.startsWith(infos[j].toUpperCase()) && infos[j].length() > 7) {
-                    cf = infos[j].toLowerCase();
-                }
-            }
-
-            // 添加列族、列名、列值
-            put.addColumn(Bytes.toBytes(cf), Bytes.toBytes(columnName), Bytes.toBytes(line[i]));
-        }
-        puts.add(put);
-        if (puts.size() >= MAX_NUM - 24) {
-            HBaseUtil.addDatas(tableName, puts);
-            puts.clear();
-        }
-
-    }
 
     /**
      * 去除首尾空格和引号
@@ -202,12 +148,6 @@ public class FileToHBase {
             arr[i] = ((st > 0) || (len < value.length)) ? arr[i].substring(st, len) : arr[i];
         }
         return arr;
-    }
-
-
-    public static void main(String[] args) {
-        String path = FileToHBase.class.getClassLoader().getResource("").getPath();
-        new FileToHBase().fileToHbase(path + "../notebook/upload/2013.csv", true);
     }
 
 }
